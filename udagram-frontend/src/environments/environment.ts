45fc775b77c9178588a7b22dc53526a0bfdf36d6
Uyên Nguyễn <uyennguyen0721@gmail.com>
2023-08-29 12:19:36 +0700

    update project and screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a269b986b9e234ee8a44a5737358f51c-604434082.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
